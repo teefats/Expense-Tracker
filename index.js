faa039a9eac843a1be482b014eacee3a1c168393
Tunde <tee_fats@yahoo.com>
2020-08-25 19:34:33 +0100

    Rectified issues

--- index.js
+++ index.js
@@ -1,50 +1,49 @@
-const balance = document.querySelector(".balance");const money_plus = document.querySelector("#money-plus");
+const balance = document.querySelector(".balance");
+const money_plus = document.querySelector("#money-plus");
 const money_minus = document.querySelector("#money-minus");
 const list = document.querySelector(".list");
-const form= document.querySelector(".form-control");const text = document.querySelector(".text");
-const amount= document.querySelector(".amount");
+const form = document.querySelector("form");
+const text = document.querySelector(".text-input");
+const amount = document.querySelector(".entry-amount");
 const btn = document.querySelector(".btn")
 
-const dummyTransactions = [
-    {id:1, text:'Flower', amount: -20  
-    },
-    {id:2, text:'Salary', amount: 300,  
-    },{id:3, text:'Book', amount: -10,  
-},{id:4, text:'Camera', amount: 150,  
-}
-];
+// const dummyTransactions = [
+//     {id:1, text:'Flower', amount: -20  
+//     },
+//     {id:2, text:'Salary', amount: 300,  
+//     },{id:3, text:'Book', amount: -10,  
+// },{id:4, text:'Camera', amount: 150,  
+// }
+// ];
 
-const localStorageTransactions = JSON.parse(localStorage.getItem('transactions'));
-
-
+const localStorageTransactions = JSON.parse(
+    localStorage.getItem('transactions')
+);
 
 // let transactions = dummyTransactions;
-let transactions = localstorage.getItem('transactions') !== null ? localStorageTransactions: [];
+let transactions =
+    localStorage.getItem('transactions') !== null ? localStorageTransactions : [];
 //Add transaction
-function addTransaction(e){
+function addTransaction(e) {
     e.preventDefault();
 
-    if(text.value.trim() === '' || amount.value.trim() === '')
-    {
-        alert('Please add text and amount');
-
-    }
-    else{
+    if (text.value.trim() === '' || amount.value.trim() === '') {
+        alert('Please add a text and amount');
+    } else {
         const transaction = {
-            id:generateID(), 
-            text:text.value,
+            id: generateID(),
+            text: text.value,
             amount: +amount.value
-
         };
         transactions.push(transaction);
         addTransactionDOM(transaction);
-        
+
         //uPDATE VALUES
         updateValues();
 
-        updateLocalStorage
+        updateLocalStorage();
         //cLEAR TRANASCTIONS
-        text.value= '';
+        text.value = '';
         amount.value = ''
     }
 }
@@ -55,7 +54,7 @@ function generateID() {
     return Math.floor(Math.random() * 100000000);
 }
 //Add transactions to dom list
-function addTransactionDOM(transaction){
+function addTransactionDOM(transaction) {
     //Get sign
     console.log("word")
     const sign = transaction.amount < 0 ? '-' : ' + ';
@@ -65,7 +64,7 @@ function addTransactionDOM(transaction){
 
     item.innerHTML = `
     ${transaction.text} <span>${sign}${Math.abs(transaction.amount)}
-    </span><button class = "delete-btn" onclick="removeTransaction(${transaction.id})>x</button>`;
+    </span><button class = "delete-btn" onclick="removeTransaction(${transaction.id})">x</button>`;
 
     list.appendChild(item);
     console.log("word")
@@ -74,7 +73,7 @@ function addTransactionDOM(transaction){
 
 //Update the balance income and expense
 
-function updateValues(){
+function updateValues() {
     const amounts = transactions.map(transaction => transaction.amount);
     const total = amounts.reduce((acc, item) => (acc += item), 0).toFixed(2);
     console.log(total);
@@ -84,14 +83,14 @@ function updateValues(){
         .reduce((acc, item) => (acc += item), 0)
         .toFixed(2);
 
-        console.log(income);
-    
-    const expense =  (amounts
+    console.log(income);
+
+    const expense = (amounts
         .filter(item => item < 0)
-        .reduce((acc, item) => (acc += item), 0) * 
+        .reduce((acc, item) => (acc += item), 0) *
         -1).toFixed(2);
-    
-        console.log(expense)
+
+    console.log(expense)
 
     balance.innerText = `£${total}`;
     money_plus.innerText = `£${income}`;
@@ -100,8 +99,9 @@ function updateValues(){
 }
 
 //remove transaction by id
-function removeTransaction(id){
+function removeTransaction(id) {
     transactions = transactions.filter(transaction => transaction.id !== id);
+    updateLocalStorage();
     init();
 }
 
@@ -111,7 +111,7 @@ function updateLocalStorage() {
 }
 //init app
 
-function init(){
+function init() {
     console.log("word")
     list.innerHTML = '';
     transactions.forEach(addTransactionDOM);
@@ -121,4 +121,4 @@ function init(){
 
 init();
 
-btn.addEventListener('submit', addTransaction);
+form.addEventListener('submit', addTransaction);
